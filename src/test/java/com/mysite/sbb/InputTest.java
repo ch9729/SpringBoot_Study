2f504f6c46626c625c02d7215a51753fc33916e6
ch9729 <karlll@naver.com>
2024-12-06 10:55:12 +0900

    H2-console -> MySQL DB로 변경

--- src/test/java/com/mysite/sbb/InputTest.java
+++ src/test/java/com/mysite/sbb/InputTest.java
@@ -1,6 +1,8 @@
 package com.mysite.sbb;
 
 import com.mysite.sbb.question.QuestionService;
+import com.mysite.sbb.user.SiteUser;
+import com.mysite.sbb.user.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,12 +13,16 @@ public class InputTest {
     @Autowired
     private QuestionService qService;
 
+    @Autowired
+    private UserService userService;
+
     @Test
     public void test() {
         for (int i = 0; i <= 300; i ++) {
             String s = String.format("테스트 데이터입니다: [%03d]", i);
             String c = "내용";
-        qService.createQuestion(s,c, null);
+            SiteUser user =userService.getUser("user");
+        qService.createQuestion(s,c, user);
         }
 
     }
